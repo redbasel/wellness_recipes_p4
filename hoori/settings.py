1894dae52b2077638bfb3876024c07fef91d59e3
redbasel <jansafar014@gmail.com>
2022-07-14 13:01:45 +0000

    2nd try deploy hoori project

--- hoori/settings.py
+++ hoori/settings.py
@@ -40,7 +40,9 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'cloudinary_storage',
     'django.contrib.staticfiles',
+    'cloudinary',
     'hoori_recipes',
 ]
 
