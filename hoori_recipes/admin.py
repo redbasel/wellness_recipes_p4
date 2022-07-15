a71b87df75b3a8801e9803947411c8c225c79487
redbasel <jansafar014@gmail.com>
2022-07-15 16:56:18 +0000

    User story '#12 view post list' now done by updating index.html,views.py,urls.py,admin.py

--- hoori_recipes/admin.py
+++ hoori_recipes/admin.py
@@ -12,7 +12,7 @@ class PostAdmin(SummernoteModelAdmin):
     summernote_fields = ('content')
 
 @admin.register(Comment)
-class CommentAdmin(admin.ModelAdmin)
+class CommentAdmin(admin.ModelAdmin):
     list_display = ('name', 'body', 'post', 'created_on', 'approved')
     list_filter = ('approved', 'created_on')
     search_fields = ('name', 'email', 'body')
--- hoori_recipes/views.py
+++ hoori_recipes/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views import generic
+from django.views import generic, View
 from .models import Post
 
 
@@ -9,3 +9,22 @@ class PostList(generic.ListView):
     template_name = "index.html"
     paginate_by = 6
 
+class PostDetail(View):
+
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comments = post.comments.filter(approved=True).order_by("-created_on")
+        liked = False
+        if post.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        return render(
+            request,
+            "post_detail.html",
+            {
+                "post": post,
+                "comments": comments,
+                "liked": liked
+            },
+        )
