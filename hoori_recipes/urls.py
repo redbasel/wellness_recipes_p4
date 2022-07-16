ae55a93c5cf9d55e0b93b793b0b9d6202dfc82cf
redbasel <jansafar014@gmail.com>
2022-07-16 09:37:09 +0000

    Solved user story #4' User can like recipe' by adding functionality to post_detail.html and urls.py

--- hoori_recipes/urls.py
+++ hoori_recipes/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
+    path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
 ]
 
